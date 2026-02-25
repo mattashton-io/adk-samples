054428e226316a50f599fec62252a5c061606c2c
Matt Ashton <mattashton@google.com>
2026-02-25 21:39:00 +0000

    fork to my GH

--- python/agents/data-science/data_science/utils/reference_guide_RAG.py
+++ python/agents/data-science/data_science/utils/reference_guide_RAG.py
@@ -27,6 +27,7 @@ print(env_file_path)
 load_dotenv(dotenv_path=env_file_path)
 
 PROJECT_ID = os.getenv("GOOGLE_CLOUD_PROJECT")
+REGION = os.getenv("GOOGLE_CLOUD_LOCATION")
 corpus_name = os.getenv("BQML_RAG_CORPUS_NAME")
 
 display_name = "bqml_referenceguide_corpus"
@@ -37,7 +38,7 @@ paths = [
 
 
 # Initialize Vertex AI API once per session
-vertexai.init(project=PROJECT_ID, location="us-central1")
+vertexai.init(project=PROJECT_ID, location=REGION)
 
 
 def create_RAG_corpus():
